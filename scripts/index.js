48b5dff96a02ea4bde5fa344b303689d9b261ca5
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-07 22:44:16 -0300

    min

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+function promocaoIndex() {
+    const ads = `<aside>
+    <div>
+        <img src="/imagens-sala/front-fachada-senac-montes-claros-sala-mineira-lagoa-dos-patos-mg.jpg" alt="">
+    </div>
+    <p>
+    A Sala Mineira do Empreendedor é o resultado da parceria entre o SEBRAE MINAS e a JUCEMG – Junta Comercial do Estado de Minas Gerais. Ela foi desenvolvida para melhorar e simplificar o ambiente de negócios em todo Estado, atuando lado a lado com as administrações municipais, oferecendo apoio para empreendimentos de todos os portes.
+    Aqui, você irá encontrar um espaço único para formalizar o seu empreendimento, obter orientações, informações e todo o conhecimento necessário para o seu desenvolvimento profissional.Tudo o que você precisa para fazer o seu negócio seguir em frente, de um jeito simples e prático.        
+    </p>
+    <img src="/imagens-sala/artigos/artigo-sebrae-compre-do-bairro-min.jpeg" alt="">
+</aside>`
+    promocoesIndex.innerHTML = ``
+ 
+
+    
+}
