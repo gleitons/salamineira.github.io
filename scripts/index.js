44c06da41162481340ee46d1e746655c0724b4e8
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-08 08:09:34 -0300

    anucn

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 function promocaoIndex() {
+    const menu = MenuPrincipal();
+    
     const ads = `<aside>
     <div>
         <img src="/imagens-sala/front-fachada-senac-montes-claros-sala-mineira-lagoa-dos-patos-mg.jpg" alt="">
@@ -9,14 +11,30 @@ function promocaoIndex() {
     </p>
     <img src="/imagens-sala/artigos/artigo-sebrae-compre-do-bairro-min.jpeg" alt="">
 </aside>`
-    promocoesIndex.innerHTML = ``
+    promocoesIndex.innerHTML = `${ads}  ${maisArtigos()} `
  
 
     
 }
 
+function MenuPrincipal() {
+    const menu = ` <nav>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/nota-fiscal">nf-e</a></li>
+                <li><a href="#">notícias</a></li>
+                <li><a href="/servicos-prefeitura-lagoa-dos-patos-mg">Serviços</a></li>
+                <li><a href="/cursos">Cursos</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="/pesquisar/">Pesquisar</a></li>
+            </nav>`
+    return menu;
+}
+
 function adsNoArtigo() {
+    const menu = MenuPrincipal();
     const adsense = Math.floor(Math.random() * 6) + 1;
+    menuPrincipal.innerHTML = menu;
 
     if (adsense == 1) {
         adsScreen = `Propaganda 01`
@@ -39,4 +57,16 @@ function adsNoArtigo() {
 
     adsArtigo.innerHTML = `${adsScreen}`
 
+    
+
+}
+
+function maisArtigos() {
+    return `<ul class="leiaMais">
+    <h3>Leia Mais</h3>
+    <li><a href="/fiv-sebrae-lagoa-dos-patos-mg/">Fiv em Lagoa dos Patos MG</a></li>
+    <li><a href="/cursos-gratuitos-senac-em-montes-claros-mg">Cursos gratuitos com 196 vagas no Senac Montes Claros MG</a></li>
+    <li><a href="/acao-social-emissao-de-carteira-de-identidade-lagoa-dos-patos-mg/">Secretaria de Assistência Social, CRAS e prefeitura municipal realiza ação social em Lagoa dos Patos MG</a></li>
+    <li><a href="/campanha-de-vacinacao-antirrabica-em-lagoa-dos-patos-mg/">Campanha de vacinação antirrábica em Lagoa dos Patos MG</a></li>
+</ul>`
 }
