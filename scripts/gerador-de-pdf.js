9740395897d93bb72cec23d6457332b4d8037ca8
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-09 18:59:28 -0300

    mei

--- scripts/gerador-de-pdf.js
+++ scripts/gerador-de-pdf.js
@@ -5,12 +5,12 @@ function generatordePDF(){
 
 // Don't forget, that there are CORS-Restrictions. So if you want to run it without a Server in your Browser you need to transform the image to a dataURL
 // Use http://dataurl.net/#dataurlmaker
+
+
 var doc = new jsPDF();
+// doc.addImage("imagens-sala/relatorio-sala.jpg", "JPEG", 55, 5, 90, 23);
 
 
-doc.setFontSize(40);
-doc.text("Octonyan loves jsPDF", 35, 25);
-doc.addImage("examples/images/Octonyan.jpg", "JPEG", 15, 40, 180, 180);
 
 
 // Empty square
