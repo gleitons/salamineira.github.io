4ac27cf8143b6726c98515636621648a274205b6
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-09 22:06:47 -0300

    MANU

--- scripts/gerador-de-pdf.js
+++ scripts/gerador-de-pdf.js
@@ -1,10 +1,25 @@
 
-
+function emManutencao() {
+    alert('ESTAMOS EM MANUTENÇÃO, EM BREVE ESTARÁ DISPONÍVEL')
+}
 
 function generatordePDF(){
 
-// Don't forget, that there are CORS-Restrictions. So if you want to run it without a Server in your Browser you need to transform the image to a dataURL
-// Use http://dataurl.net/#dataurlmaker
+const cnpj = document.getElementById('cnpj').value;
+const mei = document.getElementById('mei').value;
+const dataIni = document.getElementById('dataIni').value;
+const dataF = document.getElementById('dataF').value;
+const revendaSemNota = document.getElementById('revendaSemNota').value;
+const revendaComNota = document.getElementById('revendaComNota').value;
+const IndustrialSemNota = document.getElementById('IndustrialSemNota').value;
+const IndustrialComNota = document.getElementById('IndustrialComNota').value;
+const servicoSemNota = document.getElementById('servicoSemNota').value;
+const servicoComNota = document.getElementById('servicoComNota').value;
+const local = document.getElementById('local').value.toUpperCase();
+const datadoLocal = document.getElementById('datadoLocal').value;
+
+emManutencao();
+
 
 
 var doc = new jsPDF();
@@ -21,15 +36,15 @@ doc.setFontSize(13);
 doc.text('RELATÓRIO MENSAL DAS RECEITAS BRUTAS', 105, 38, null, null, "center")
 doc.rect(10, 40, 190, 10);
 doc.setFontSize(12);
-doc.text('CNPJ:', 12, 48)
+doc.text(`CNPJ: ${cnpj}`, 12, 48)
 
 doc.rect(10, 50, 190, 10);
 doc.setFontSize(12);
-doc.text('Empreendedor individual:', 12, 58)
+doc.text(`Empreendedor individual: ${mei}`, 12, 58)
 
 doc.rect(10, 60, 190, 10);
 doc.setFontSize(12);
-doc.text('Período de apuração:', 12, 68)
+doc.text(`Período de apuração: de ${dataIni} a ${dataF}`, 12, 68)
 
 doc.setDrawColor(0);
 doc.setFillColor(220, 220, 220);
@@ -42,14 +57,14 @@ doc.setFontSize(12);
 doc.text('I – Revenda de mercadorias com dispensa de emissão de documento fiscal', 12, 88)
 doc.rect(160, 80, 40, 10);
 doc.setFontSize(12);
-doc.text('R$', 162, 88)
+doc.text(`R$ ${revendaSemNota.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`, 162, 88)
 
 doc.rect(10, 90, 150, 10);
 doc.setFontSize(12);
 doc.text('II – Revenda de mercadorias com documento fiscal emitido', 12, 98)
 doc.rect(160, 90, 40, 10);
 doc.setFontSize(12);
-doc.text('R$', 162, 98)
+doc.text(`R$ ${revendaComNota}`, 162, 98)
 
 doc.rect(10, 100, 150, 10);
 doc.setFontSize(12);
@@ -71,21 +86,21 @@ doc.setFontSize(12);
 doc.text('documento fiscal', 12, 133)
 doc.rect(160, 120, 40, 15);
 doc.setFontSize(12);
-doc.text('R$', 162, 133)
+doc.text(`R$ ${IndustrialSemNota}`, 162, 133)
 
 doc.rect(10, 135, 150, 10);
 doc.setFontSize(12);
 doc.text('V – Venda de produtos industrializados com documento fiscal emitido', 12, 143)
 doc.rect(160, 135, 40, 10);
 doc.setFontSize(12);
-doc.text('R$', 162, 143)
+doc.text(`R$ ${IndustrialComNota}`, 162, 143)
 
 doc.rect(10, 145, 150, 10);
 doc.setFontSize(12);
 doc.text('VI – Total das receitas com venda de produtos industrializados (IV + V)', 12, 153)
 doc.rect(160, 145, 40, 10);
 doc.setFontSize(12);
-doc.text('R$', 162, 153)
+doc.text(`R$ ${IndustrialComNota}`, 162, 153)
 
 doc.setDrawColor(0);
 doc.setFillColor(220, 220, 220);
@@ -100,35 +115,35 @@ doc.setFontSize(12);
 doc.text('documento fiscal', 12, 175)
 doc.rect(160, 165, 40, 15);
 doc.setFontSize(12);
-doc.text('R$', 162, 173)
+doc.text(`R$ ${servicoSemNota}`, 162, 173)
 
 doc.rect(10, 180, 150, 10);
 doc.setFontSize(12);
 doc.text('VIII – Receita com prestação de serviços com documento fiscal emitido', 12, 188)
 doc.rect(160, 180, 40, 10);
 doc.setFontSize(12);
-doc.text('R$', 162, 188)
+doc.text(`R$ ${servicoComNota}`, 162, 188)
 
 doc.rect(10, 190, 150, 10);
 doc.setFontSize(12);
 doc.text('IX – Total das receitas com prestação de serviços (VII + VIII)', 12, 198)
 doc.rect(160, 190, 40, 10);
 doc.setFontSize(12);
-doc.text('R$ 81.000,00', 162, 198)
+doc.text(`R$ ${IndustrialComNota}`, 162, 198)
 
 doc.rect(10, 200, 150, 10);
 doc.setFontSize(12);
 doc.text('IX – Total das receitas com prestação de serviços (VII + VIII)', 12, 208)
 doc.rect(160, 200, 40, 10);
 doc.setFontSize(12);
-doc.text('R$ 81.000,00', 162, 208)
+doc.text(`R$ ${IndustrialComNota}`, 162, 208)
 
 doc.rect(10, 210, 150, 25);
 doc.setFontSize(12);
 doc.text('LOCAL E DATA:', 12, 218)
 doc.rect(160, 210, 40, 25);
 doc.setFontSize(12);
-doc.text('R$', 162, 208)
+doc.text(`${local}, ${datadoLocal}`, 12, 228)
 
 doc.rect(10, 235, 190, 25);
 doc.setFontSize(12);
@@ -140,7 +155,7 @@ doc.text('- As notas fiscais relativas às operações ou prestações realizada
 
 
 
-doc.save('a4.pdf')
+// doc.save('a4.pdf')
 
 
 
