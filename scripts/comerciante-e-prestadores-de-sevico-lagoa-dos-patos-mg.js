ff78d88455df6ba7bf7621fdce08aa6814f22cb1
HD Os <hdos@iMac-de-HD.local>
2022-03-27 15:30:49 -0300

    anuncios

--- scripts/comerciante-e-prestadores-de-sevico-lagoa-dos-patos-mg.js
+++ scripts/comerciante-e-prestadores-de-sevico-lagoa-dos-patos-mg.js
@@ -4,7 +4,7 @@ anuncios.forEach(function (anuncio) {
     console.log(anuncio.nome);
     document.getElementById('anunciantes').innerHTML += `<a href="${anuncio.link}" target = "_blank">
     <div class="comerciantePrestador">
-        <img src="${anuncio.imagem}" alt="">
+        <img class="imgC" src="${anuncio.imagem}" alt="">
         <h2>${anuncio.nome}</h2>
         <p>${anuncio.tipo}</p>
         <p>${anuncio.area}</p>
@@ -23,7 +23,7 @@ function comerciantesPrestadores() {
         {
             'imagem': 'https://www.asconstrutora.com.br/images/as-construtora-logo-andeilton-soares-de-souza-ltda.png',
             'nome': 'AS CONSTRUTORA LTDA',
-            'link': 'http://127.0.0.1:5500/as-construtora/',
+            'link': '/as-construtora/',
             'tipo': 'Prestador de Serviços',
             'area': 'Construção e reforma'
         },
@@ -56,21 +56,35 @@ function comerciantesPrestadores() {
             'area': 'Engenharia Elétrica'
         },
         {
-            'imagem': 'https://www.salamineira.com/img/restaurante-prato-nobre-recanto-nobre-lagoa-dos-patos-mg-sonia-magalhaes-restaurante.jpeg',
+            'imagem': 'https://www.naturfive.com/imagens-naturfive/restaurante-prato-nobre.jpeg',
             'nome': 'RESTAURANTE PRATO NOBRE',
-            'link': 'https://www.salamineira.com/duarte-engenharia/',
+            'link': '/restaurante-prato-nobre/',
             'tipo': 'Comerciante',
             'area': 'Restaurante e Lanchonete'
         },
         {
             'imagem': 'https://www.salamineira.com/imagens-sala/2022/02/logo-tijolego-home.jpeg',
             'nome': 'TIJOLEGO',
-            'link': 'https://www.salamineira.com/duarte-engenharia/',
+            'link': '/tijolego.html',
             'tipo': 'Comerciante',
             'area': 'Tijolos ecológicos'
         },
         {
-            'imagem': 'https://static.wixstatic.com/media/7343dd_af04788053004d29a4004c2e8e8bdb3b~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01/divulgue-seu-negocio-sala-mineira-empree.jpg',
+            'imagem': 'https://www.salamineira.com/imagens-sala/2022/03/hotel-estrela-lagoa-dos-patos-mg.jpeg',
+            'nome': 'HOTEL ESTRELA LAGOA DOS PATOS MG',
+            'link': 'https://www.salamineira.com/hotel-em-lagoa-dos-patos-mg.html',
+            'tipo': 'Prestador de Serviços',
+            'area': 'Hotel e Hospedagem'
+        },
+        {
+            'imagem': 'https://www.salamineira.com/imagens-sala/2022/03/hotel-cosmos-capa.png',
+            'nome': 'HOTEL COSMOS LAGOA DOS PATOS MG',
+            'link': 'https://www.salamineira.com/imagens-sala/2022/03/hotel-cosmos-capa.png',
+            'tipo': 'Prestador de Serviços',
+            'area': 'Hotel e Hospedagem'
+        },
+        {
+            'imagem': 'https://img.elo7.com.br/product/zoom/258B7CB/adesivo-parede-restaurante-prato-feito-comida-caseira-lenha-adesivo-restaurante-fritas-salada.jpg',
             'nome': 'ANUNCIE GRÁTIS',
             'link': 'https://www.salamineira.com/divulgue-aqui/',
             'tipo': 'Anuncie Grátis',
