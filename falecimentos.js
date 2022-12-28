803c25553a2c7175d6803fe2291c55ee9798752d
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2022-12-28 07:38:44 -0300

    fal

---
+++ falecimentos.js
@@ -0,0 +1,29 @@
+{
+    
+    "1"= { 
+        "nome": "ADAO GONCALVES DOS SANTOS",
+        "nascimento": "",
+        "falecimento": "17/01/2018",
+        "nota": "Pessoa de fé e muito mais...",
+        "obito": "Não identificado",
+        "mãe": "ANDREZA AVELINA DA SILVA",
+        "natural": "Lagoa dos Patos - MG",
+        "estadoFalecimento": "Minas Gerais",
+        "LocalFalecimento": "Lagoa dos Patos",
+        "estado": "MG"
+     },
+    "0"=
+    {  "nome": "Adão Messias",
+    "nascimento": "01/07/2000",
+    "falecimento": "01/03/2010",
+    "nota": "Pessoa de fé e muito mais...",
+    "obito": "Não identificado",
+    "mãe": "Nome da Mãe",
+    "natural": "Lagoa dos Patos - MG",
+    "estadoFalecimento": "Minas Gerais",
+    "LocalFalecimento": "Lagoa dos Patos MG",
+    "estado": "MG" }
+
+    
+}
+
