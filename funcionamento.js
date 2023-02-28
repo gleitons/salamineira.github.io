149dd1f08fdc222556c4326e9e7c22384b602edd
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-02-28 17:08:02 -0300

    empresas

--- funcionamento.js
+++ funcionamento.js
@@ -1,19 +1,25 @@
 const data = new Date();
-const diaD = data.getDay()
+var diaD = data.getDay()
 const dia = data.getDate()
-const mes = data.getMonth()+1
+const mes = data.getMonth() + 1
 const ano = data.getFullYear()
+var feriados = ["20/1/2023", "20/2/2023", "21/2/2023", "22/2/2023", "1/3/2023", "7/4/2023", "21/4/2023", "1/5/2023", "8/6/2023", "7/9/2023", "12/10/2023", "2/11/2023", "15/11/2023", "25/12/2023", "26/12/2023", "27/12/2023", "28/12/2023", "29/12/2023", "30/12/2023", "31/12/2023", "1/1/2024"]
 
 const anoCompara = `${dia}/${mes}/${ano}`
 //
-
-const feriados  = ["20/1/2023", "20/2/2023", "21/2/2023", "22/2/2023", "1/3/2023", "7/4/2023", "21/4/2023", "1/5/2023", "8/6/2023", "7/9/2023", "12/10/2023", "2/11/2023", "15/11/2023", "25/12/2023","26/12/2023","27/12/2023","28/12/2023","29/12/2023","30/12/2023","31/12/2023","1/01/2024"]
 const funciona = document.querySelector('#funciona');
 
-for (let index = 0; index < feriados.length; index++) {
-   
 
-    if (diaD == 6 || diaD == 0){
+
+
+
+
+
+for (let index = 0; index < feriados.length; index++) {
+
+
+    if (diaD == 6 || diaD == 0) {
+
         funciona.innerHTML = `<div class="funcionamento cvermelho">
         <div >
             <i class="bi bi-emoji-frown-fill"></i>
@@ -21,8 +27,8 @@ for (let index = 0; index < feriados.length; index++) {
         </div>
 
     </div>`
-        
-    } 
+
+    }
     if (anoCompara == feriados[index]) {
         funciona.innerHTML = `<div class="funcionamento cvermelho">
         <div >
@@ -31,13 +37,25 @@ for (let index = 0; index < feriados.length; index++) {
             <button><a href="/horario-de-funcionamento.html">Veja o calendário completo</a></button>
         </div>
     </div>`
-    } 
-    
-    
+    }
+
+
 }
 const funcionaAmanha = document.querySelector('#funcionaAmanha');
 const diaA = data.getDay() + 1
 const diaB = data.getDate() + 1
+
+console.log(mes)
+
+if(mes == 2 && diaB == 29 || diaB == 30 || diaB == 31){
+    funcionaAmanha.innerHTML = `<div class="funcionamento cvermelho">
+        <div >
+            <i class="bi bi-emoji-frown-fill"></i>
+            <p>Amanhã <strong>NÃO</strong> estaremos funcionando - Feriado</p>
+        </div>
+    </div>`
+}
+
 
 const diaSeguinte = `${diaB}/${mes}/${ano}`
 console.log(diaB)
@@ -45,17 +63,17 @@ console.log(diaB)
 
 
 
-for (let i = 0; i < feriados.length; i++) {    
+for (let i = 0; i < feriados.length; i++) {
 
-    if (diaA == 6 || diaA == 0 || diaA == 7){
+    if (diaA == 6 || diaA == 0 || diaA == 7) {
         funcionaAmanha.innerHTML = `<div class="funcionamento cvermelho">
         <div >
             <i class="bi bi-emoji-frown-fill"></i>
             <p>Amanhã <strong>NÃO</strong> Iremos funcionar - Bom descanso</p>
         </div>
     </div>`
-        
-    } 
+
+    }
     if (diaSeguinte == feriados[i]) {
         funcionaAmanha.innerHTML = `<div class="funcionamento cvermelho">
         <div >
@@ -63,9 +81,9 @@ for (let i = 0; i < feriados.length; i++) {
             <p>Amanhã <strong>NÃO</strong> estaremos funcionando - Feriado</p>
         </div>
     </div>`
-    } 
-    
-    
+    }
+
+
 }
 
 
