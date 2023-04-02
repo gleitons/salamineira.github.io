4f5c828e77befcbba9d4011a8c3a098fcce1194d
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-04-02 19:00:24 -0300

    ads

---
+++ terra-nua/informacao.js
@@ -0,0 +1,135 @@
+
+
+function gerarNomes(anoS) {
+   // const url2016 = `./cidades-mg-${anoS}`
+    const cidadeC = document.querySelector('#cidadeC')
+    const response = localStorage.getItem(`city${anoS}`);
+    const data = JSON.parse(response)
+
+    data.map((nomeCity) => {
+        const seletorCidade = document.createElement('option');
+        seletorCidade.setAttribute('value', nomeCity.nome);
+        seletorCidade.textContent = nomeCity.nome;
+        cidadeC.appendChild(seletorCidade)
+
+
+
+    })
+    
+}
+
+async function avaliacoes(anoS) {
+    const url = `./cidades-mg-${anoS}`
+    console.log(url)
+    const cidadeCC = document.querySelector('#cidadeC')
+    var cidadeC = cidadeCC.options[cidadeCC.selectedIndex].value
+
+    const tabelaAvaliacaoVenal = document.querySelector('#tabelaAvaliacaoVenal')
+    const response = await fetch(url)
+    var data = await response.json()
+
+    //console.log(data)
+
+    if(localStorage.getItem(`city${anoS}`) == null){
+        
+        localStorage.setItem(`city${anoS}`, JSON.stringify(data))
+    }
+
+    data.map((city) => {
+        if(cidadeC == city.nome) {
+            const aptBoa = city.aptidaoBoa
+        tabelaAvaliacaoVenal.innerHTML = `
+        <h2>Preço por Ha (hectare)</h2>
+        <div style="height:45px;">
+        <img src="./imagens/emater-mg-logo-6FDB59CBDA-seeklogo.com_.png.webp" alt="">
+    </div>
+    <h2>Valor de terra nua do município de ${city.nome}</h2>
+    <tr>
+        <tr>Ano</tr>   
+                 
+    </tr>
+    <tr>
+        <span class="selecioneAn">
+            <tr>${city.ano}</tr>  
+        </span>
+    </tr><table class="tabelaS">
+    <thead class="cabeca">       
+    </thead>
+    <tbody class="corpo">
+        
+        <tr>
+            <td>1º:</td>
+            <td>Lavoura aptidão boa</td>
+            <td>R$ ${aptBoa.replace(/"/g, '')}</td>
+        </tr>
+        <tr>
+            <td>2º:</td>
+            <td>Lavoura aptidão regular</td>
+            <td>R$ ${city.aptdaoRegular.replace(/"/g, '')}</td>
+        </tr>
+        <tr>
+            <td>3º:</td>
+            <td>Lavoura aptidão restrita</td>
+            <td>R$ ${city.aptdaoRestrita.replace(/"/g, '')}</td>
+        </tr>
+        <tr>
+            <td>4º:</td>
+            <td>Pastagem plantada</td>
+            <td>R$ ${city.pastagemPlantada.replace(/"/g, '')}</td>
+        </tr>
+        
+        <tr>
+            <td>5º:</td>
+            <td>Silvicultura ou Pastagem
+                Natural</td>
+                <td>R$ ${city.pastagemNatural}</td>
+        </tr>
+        <tr>
+            <td>6º:</td>
+            <td>Preservação da Fauna ou
+                Flora</td>
+                <td>R$ ${city.reserva}</td>
+        </tr>
+    </tbody>
+    <tfoot>
+       <tr>
+        <td></td>
+       </tr>
+            
+       
+    </tfoot>
+    <p class="cidadeInfo">${city.nome.toUpperCase()}</p>
+</table>
+<p class="cidadeDown" onclick="esconde()" >Veja a Assinatura da Reunião em ${city.ano} em Lagoa dos Patos - MG</p>
+
+<p >Baixe a planilha completa - <a href="./arquivos/valor-terra-nua-${anoS}.pdf#">Clique Aqui</a></p>
+<div class="assinaturaTermo ocultaEsome" id="imgOclude">
+    <img src="./img/terra-nua-${anoS}.jpg" onclick="esconde()" style="cursor:pointer;" alt="Valor de terra nua do município de ${city.nome} no ano de ${city.ano}">
+</div>`   
+        }       
+        
+    })
+ gerarNomes(anoS)
+ 
+  
+
+    
+}
+
+function esconde() {
+    const imgOclude = document.querySelector('#imgOclude')
+    imgOclude.classList.toggle('ocultaEsome')
+}
+
+function gerarMenu() {
+    var menuTopInfo = document.querySelector('#menuTopInfo')
+
+    for(let i = 2015; i <= 2023; i++) {
+        menuTopInfo.innerHTML += `<a href="./venal-${i}.html">
+        <li>${i}</li>
+        </a>`
+    }
+   
+
+}
+gerarMenu()
