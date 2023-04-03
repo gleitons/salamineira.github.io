5d0400e83cb52ac049187d03e08b94ffcbf2cbf3
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-04-03 07:49:03 -0300

    ads

--- terra-nua/informacao.js
+++ terra-nua/informacao.js
@@ -70,70 +70,73 @@ async function avaliacoes(anoS) {
         <div style="height:45px;">
         <img src="./imagens/emater-mg-logo-6FDB59CBDA-seeklogo.com_.png.webp" alt="">
     </div>
-    <h2>Valor de terra nua do município de ${city.nome}</h2>
-    <tr>
-        <tr>Ano</tr>   
-                 
-    </tr>
-    <tr>
-        <span class="selecioneAn">
-            <tr>${city.ano}</tr>  
-        </span>
-    </tr><table class="tabelaS">
-    <thead class="cabeca">       
-    </thead>
-    <tbody class="corpo">
+    <div class="apareceInfo">
+        <h2>Valor de terra nua do município de ${city.nome}</h2>
+        <tr>
+            <tr>Ano</tr>
         
-        <tr>
-            <td>1º:</td>
-            <td>Lavoura aptidão boa</td>
-            <td>R$ ${aptBoa.replace(/"/g, '')}</td>
         </tr>
         <tr>
-            <td>2º:</td>
-            <td>Lavoura aptidão regular</td>
-            <td>R$ ${city.aptdaoRegular.replace(/"/g, '')}</td>
+            <span class="selecioneAn">
+                <tr>${city.ano}</tr>
+            </span>
         </tr>
-        <tr>
-            <td>3º:</td>
-            <td>Lavoura aptidão restrita</td>
-            <td>R$ ${city.aptdaoRestrita.replace(/"/g, '')}</td>
-        </tr>
-        <tr>
-            <td>4º:</td>
-            <td>Pastagem plantada</td>
-            <td>R$ ${city.pastagemPlantada.replace(/"/g, '')}</td>
-        </tr>
+        <table class="tabelaS">
+        <thead class="cabeca">
+        </thead>
+        <tbody class="corpo">
         
-        <tr>
-            <td>5º:</td>
-            <td>Silvicultura ou Pastagem
-                Natural</td>
-                <td>R$ ${city.pastagemNatural}</td>
-        </tr>
-        <tr>
-            <td>6º:</td>
-            <td>Preservação da Fauna ou
-                Flora</td>
-                <td>R$ ${city.reserva}</td>
-        </tr>
-    </tbody>
-    <tfoot>
-       <tr>
-        <td></td>
-       </tr>
-            
-       
-    </tfoot>
-    <p class="cidadeInfo">${city.nome.toUpperCase()}</p>
-</table>
-<p class="cidadeDown" onclick="esconde()" >Veja a Assinatura da Reunião em ${city.ano} em Lagoa dos Patos - MG</p>
-
-<p >Baixe a planilha completa - <a href="./arquivos/valor-terra-nua-${anoS}.pdf#">Clique Aqui</a></p>
-<p class="curiosidadeS" ><strong>*CURIOSIDADE:</strong> Você sabia que a cidade com maior valor da terra nua em <strong>${city.ano} foi ${agoraN[0].nome}</strong>, com valor venal de aptidão Boa no valor de R$ <strong>${agoraN[0].aptidaoBoa} mil reais</strong></p>
-<div class="assinaturaTermo ocultaEsome" id="imgOclude">
-    <img src="./img/terra-nua-${anoS}.jpg" onclick="esconde()" style="cursor:pointer;" alt="Valor de terra nua do município de ${city.nome} no ano de ${city.ano}">
-</div>`   
+            <tr>
+                <td>1º:</td>
+                <td>Lavoura aptidão boa</td>
+                <td>R$ ${aptBoa.replace(/"/g, '')}</td>
+            </tr>
+            <tr>
+                <td>2º:</td>
+                <td>Lavoura aptidão regular</td>
+                <td>R$ ${city.aptdaoRegular.replace(/"/g, '')}</td>
+            </tr>
+            <tr>
+                <td>3º:</td>
+                <td>Lavoura aptidão restrita</td>
+                <td>R$ ${city.aptdaoRestrita.replace(/"/g, '')}</td>
+            </tr>
+            <tr>
+                <td>4º:</td>
+                <td>Pastagem plantada</td>
+                <td>R$ ${city.pastagemPlantada.replace(/"/g, '')}</td>
+            </tr>
+        
+            <tr>
+                <td>5º:</td>
+                <td>Silvicultura ou Pastagem
+                    Natural</td>
+                    <td>R$ ${city.pastagemNatural}</td>
+            </tr>
+            <tr>
+                <td>6º:</td>
+                <td>Preservação da Fauna ou
+                    Flora</td>
+                    <td>R$ ${city.reserva}</td>
+            </tr>
+        </tbody>
+        <tfoot>
+           <tr>
+            <td></td>
+           </tr>
+        
+        
+        </tfoot>
+        <p class="cidadeInfo">${city.nome.toUpperCase()}</p>
+        </table>
+        <p class="cidadeDown" onclick="esconde()" >CLIQUE AQUI - Veja a Assinatura da Reunião em ${city.ano} em Lagoa dos Patos - MG</p>
+        
+        <p >Baixe a planilha completa - <a href="./arquivos/valor-terra-nua-${anoS}.pdf#">Clique Aqui</a></p>
+        <p class="curiosidadeS" ><strong>*CURIOSIDADE:</strong> Você sabia que a cidade com maior valor da terra nua em <strong>${city.ano} foi ${agoraN[0].nome}</strong>, com valor venal de aptidão Boa no valor de R$ <strong>${agoraN[0].aptidaoBoa} mil reais</strong></p>
+        <div class="assinaturaTermo ocultaEsome" id="imgOclude">
+        <img src="./img/terra-nua-${anoS}.jpg" onclick="esconde()" style="cursor:pointer;" alt="Valor de terra nua do município de ${city.nome} no ano de ${city.ano}">
+        </div>
+    </div>`   
         }       
         
     })
