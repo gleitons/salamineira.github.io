4d6782bf0b9c71a723877979c01d4eedc6993d3b
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-04-07 09:08:50 -0300

    atualização App

--- guia-turistico-lagoa-dos-patos-mg/guia.js
+++ guia-turistico-lagoa-dos-patos-mg/guia.js
@@ -98,7 +98,7 @@ async function empresas() {
     data.map((empresaL, index) => {
         infoVisivel.innerHTML += `
         <div class="btnEmpresa" onclick="verEmpresaShow(${index})">
-            <li><span>${empresaL.nomeFantasia}</span> ${empresaL.razaoSocial} - ${empresaL.cnpjN} </li>
+            <li><span>${empresaL.nomeFantasia}</span> <br> ${empresaL.razaoSocial} <br> ${empresaL.cnpjN} </li>
         </div>`
         // console.log(empresaL.id.length)
         // for(let i = 0; i < empresaL.id.length; i++ ) {
@@ -112,15 +112,69 @@ async function empresas() {
     })
 
 }
+async function localizaEmpresas() {
+    infoVisivel.innerHTML = ''
+    
+    const inicio = document.querySelector('#inicio')
+    //inicio.remove()
+    const response = await fetch(urlEmpresas);
+    const data = await response.json();
+    data.map((empresaL, index) => {
+        var nomeChave = []
+        console.log(empresaL.id.length)
+        for(let i = 0; i < empresaL.id.length; i++ ) {
+            
+            nomeChave.push(empresaL.id[i])
+        }
+        console.log(nomeChave)
+        infoVisivel.innerHTML += `
+        <div class="btnEmpresa" onclick="verEmpresaShow(${index})">
+            <h6>Clique para ver</h6>
+            <li><span>${empresaL.nomeFantasia}</span> <br> ${empresaL.razaoSocial} <br> ${nomeChave} </li>
+        </div>`
+        
+       
+    })
+
+}
+
+async function maisEmpresas() {
+    infoVisivel.innerHTML = ''
+    
+    const inicio = document.querySelector('#inicio')
+    //inicio.remove()
+    const response = await fetch(urlEmpresas);
+    const data = await response.json();
+    data.map((empresaL, index) => {
+        var nomeChave = []
+        console.log(empresaL.id.length)
+        for(let i = 0; i < empresaL.id.length; i++ ) {
+            
+            nomeChave.push(empresaL.id[i] + " ")
+        }
+        console.log(nomeChave)
+        if(empresaL.atividadePrincipal != "empresaL.atividadePrincipal") {
+        infoVisivel.innerHTML += `
+        <div class="btnEmpresa" onclick="verEmpresaShow(${index})">
+            <h6>Clique para ver</h6>
+            <li><span>${empresaL.atividadePrincipal}</span> </li>
+        </div>`
+        }
+        
+       
+    })
+
+}
+
 
 
 
 function inicioApp() {
-    infoVisivel.innerHTML = `  <h3>MODELO DE APP EMPRESA LAGOA DOS PATOS MG</h3>
+    infoVisivel.innerHTML = ` <!-- <h3>MODELO DE APP EMPRESA LAGOA DOS PATOS MG</h3>
     <div id="inicio">
         <img src="./imgguia/brasao-lagoa.jpg" alt="">
     </div>
-    <h4>brasão de Lagoa dos Patos MG - Fonte: Wikipédia</h4>`
+    <h4>brasão de Lagoa dos Patos MG - Fonte: Wikipédia</h4>-->`
 }
 
 function fechaEmpresaL() {
@@ -145,6 +199,14 @@ async function verEmpresaShow(numero) {
             <h3>${data[numero].cnpjN}</h3>
         </div>
         <div>
+            <div class="contatoEmpresa">
+                <a href="tel:${data[numero].telefone.replace(/ /g, '').replace(/-/g, '')}">
+                <img src="./imgguia/whatsapp.png" alt="">
+
+                    <h4>${data[numero].telefone}</h4>
+                </a>
+
+            </div>
             <div class="contatoEmpresa">
                 <a href="tel:${data[numero].telefone.replace(/ /g, '').replace(/-/g, '')}">
                 <img src="./imgguia/telefone-velho.png" alt="">
@@ -181,3 +243,43 @@ async function verEmpresaShow(numero) {
     </div>
 </div>`
 }
+
+function buscaEmpresa() {
+    const mostraSer = document.querySelector('#infoVisivel');
+    
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('btnEmpresa');
+
+    console.log(input.length)
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input) || input.length <= 0) {
+            x[i].style.display="none";
+            //mostraSer.style.display = "none"
+           
+        }
+        else {
+            x[i].style.display="list-item";   
+            mostraSer.style.display = "block"              
+        }
+    }
+}
+
+function fotosLagoa() {
+    infoVisivel.innerHTML = ''
+    const mostraI = document.querySelector('#infoVisivel')
+
+    console.log(mostraI)
+
+const imagensOne = ['Barragem cheia Lagoa dos Patos - MG. -27_03_2012_120.jpg', 'Barragem cheia Lagoa dos Patos - MG. -27032012.jpg',  'Barragem-lagoa-dos-patos-mg-27032012121.jpg', 'coletanea-FOTOS-de-lagoa-dos-patos-MG-Sala-mineira.jpg', 'HDR-Lagoa-dos-patos-mg-barragem.png', 'Lagoa-dos-patos-mg-barragem-cheia-27_03_2012122.jpg', 'Tim, Rodrigo, Edson, Cassio, Valdo - mortal Kombat(9).jpg']
+
+for(let i = 0; i < imagensOne.length; i++){
+    mostraI.innerHTML += `<div class="tamanhosImg">
+        <div class="Sirv" data-effect="zoom">
+            <img data-src="https://rogresph.sirv.com/sala-mineira/fotos/${imagensOne[i]}">
+        </div>
+    </div>`
+}   
+}
+
