e1ff4e258ee1a98b69cdb8b5681dda160a45b019
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-04-28 06:44:27 -0300

    atualizacao edital

--- vereadores-de-lagoa-dos-patos-mg/js.js
+++ vereadores-de-lagoa-dos-patos-mg/js.js
@@ -20,8 +20,10 @@ async function carregaParlamentar(numb) {
 
     const infoNoLegislativo = document.querySelector('#infoNoLegislativo')
     const projetosPoliticos = document.querySelector('#projetosPoliticos')
+    infoNoLegislativo.innerHTML = ''
+    projetosPoliticos.innerHTML = ''
 
-    console.log(data[numb].legislativo[0])
+    
 
     for(let i=0; i < data[numb].legislativo.length; i++){
         infoNoLegislativo.innerHTML += `<div class="cardInfoLegislativo">
@@ -33,13 +35,17 @@ async function carregaParlamentar(numb) {
 
     for (let index = 0; index < data[numb].projetos.length; index++) {
 
+
+
         projetosPoliticos.innerHTML += `<div class="infoState">
         <span>&#9745;</span>
         <div>
             <h3>Requerimento nº ${data[numb].projetos[index].numero}/${data[numb].projetos[index].ano}</h3>
             <p>Requerimento | Data: ${data[numb].projetos[index].data}</p>
-            <p>Ementa: ${data[numb].projetos[index].informacoes}</p>
-            <a href="#" target="_blank">
+            <p>Ementa: ${data[numb].projetos[index].nomeinfo}</p>
+            <p>Informação: ${data[numb].projetos[index].informacao}</p>
+            
+            <a href="${data[numb].projetos[index].download}" target="_blank">
                 <button>Saiba Mais</button>
             </a>
         </div>
@@ -57,4 +63,45 @@ async function carregaParlamentar(numb) {
     
 }
 
-carregaParlamentar(0)
+//carregaParlamentar(0)
+
+
+const todosVereadoresOn = async () => {
+    const response = await fetch('./parlamentares');
+    const data = await response.json();
+
+    const todosVereadoresOn = document.querySelector('#todosVereadoresOn')
+
+    data.map((parlamentar) => {
+        todosVereadoresOn.innerHTML +=`<div class="cardVereador">
+        <img src="${parlamentar.fotomin}" alt="">
+        <div>
+            <p>${parlamentar.nomeurna}</p>
+        </div>
+    </div>`
+    })
+}
+
+const todosParlamentaresOn = async () => {
+    const response = await fetch('./parlamentares');
+    const data = await response.json();
+
+    const classificacaoParlamentares = document.querySelector('#classificacaoParlamentares')
+
+    data.map((parlamentar, index) => {
+        classificacaoParlamentares.innerHTML +=`
+        <div onclick="carregaParlamentar(${index})" class="presidenteDacamara">
+                            <div class="fotoPresident">
+                                <img id="fotoPresidente" src="${parlamentar.fotomin}"
+                                    alt="">
+                            </div>
+                            <div>
+                                <h2>${parlamentar.nome}</h2>
+                                <p>${parlamentar.posicao}</p>
+                            </div>
+                        </div>`
+    })
+}
+
+todosVereadoresOn()
+todosParlamentaresOn()
