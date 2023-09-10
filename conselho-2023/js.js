a05699f3c2ed14fd490916accb8d697ea47f8475
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-09-10 18:53:20 -0300

    show

--- conselho-2023/js.js
+++ conselho-2023/js.js
@@ -44,6 +44,7 @@ const verResultadoVotacao = window.location.hash
 if (verResultadoVotacao == "#resultadourna") {
     fechaVotos()
 
+
 }
 
 
@@ -229,12 +230,11 @@ async function Votados() {
     const datad = await response.json()
     const votoreal = await JSON.parse(localStorage.getItem('votos'))
 
-    var somaVotos = 1500
+    
 
 
     var todosVotos = []
     for (let index = 0; index < datad.length; index++) {
-
         const candiD = {
             "nome": datad[index].nome,
             "nomecompleto": datad[index].nomecompleto,
@@ -243,18 +243,8 @@ async function Votados() {
             "votos": JSON.stringify(votoreal[index]).replace(/[^0-9]/gi, '')
         }
         todosVotos.push(candiD)
-
-
     }
-
-
-
     const data = todosVotos
-
-
-
-
-
     data.sort(function (a, b) {
         if (parseInt(a.votos) > parseInt(b.votos)) {
             return -1;
@@ -265,9 +255,7 @@ async function Votados() {
         // a must be equal to b
         return 0;
     });
-
-
-
+    var somaVotos = data[0].votos * 2
     data.map((c, index) => {
         const porc = Math.floor((c.votos * 100) / somaVotos)
         if (porc > 10) {
@@ -285,9 +273,6 @@ async function Votados() {
         <p class="colocacao">${posit}</p>
         <img class="vencedor" style="display:${resultadoPost};" src="./vencedor-min.png" alt="vencedor">
     </div>`
-
-
-
     })
     document.querySelector('.votos').innerHTML += `<h3 class="numeroUrnaH2">ESTAMOS AGUARDANDO A LIBERAÇÃO DA ENQUETE</h3><button onclick="loadAtualiza()" >Clique aqui para atualizar</button>
     `
@@ -299,9 +284,9 @@ function fechaVotos() {
     document.querySelector('#votosOn').classList.toggle('dnone')
     Votados()
     setTimeout('atencao()', 1000)
+   
 
 }
-console.log(localStorage.getItem('aceitatermos'))
 if (localStorage.getItem('aceitatermos') == 'sim') {
     document.querySelector('#termos').classList.add('dnone')
 }
@@ -455,10 +440,10 @@ function salvaHorarioVotado() {
 
 function atencao() {
     const faceCandMostra = document.querySelectorAll('.faceCandidato')
-    console.log(faceCandMostra.length)
+  
     for (let i = 0; i < faceCandMostra.length; i++) {
         faceCandMostra[i].addEventListener('click', () => {
-            console.log('ver')
+          
             document.querySelector('#fechavv').classList.add('avisaS')
             document.querySelector('#fechavv').style.backgroundColor = 'red'
         })
@@ -475,3 +460,210 @@ function atencao() {
 //     document.querySelector('#anonimo').classList.remove('dnone')
 //     console.log("Aba anônima");
 // });
+
+
+
+// function fogos() {
+//     //<![CDATA[
+// // Fuegos artificiales
+// var bits=200; // Número de pontos
+// var intensity=10; // Intensidade da explosao (recomendado entre 3 e 10)
+// var speed=30; // Velocidade (qnto menor for o numero, mais rapido)
+// var colours=new Array("#03f", "#f03", "#0e0", "#93f", "#0cc", "#f93");
+// // Cores dos fogos
+
+// var dx, xpos, ypos, bangheight;
+// var Xpos=new Array();
+// var Ypos=new Array();
+// var dX=new Array();
+// var dY=new Array();
+// var decay=new Array();
+// var colour=0;
+// var swide=800;
+// var shigh=600;
+// function write_fire() {
+// var b, s;
+// b=document.createElement("div");
+// s=b.style;
+// s.position="absolute";
+// b.setAttribute("id", "bod");
+// document.body.appendChild(b);
+// set_scroll();
+// set_width();
+// b.appendChild(div("lg", 3, 4));
+// b.appendChild(div("tg", 2, 3));
+// for (var i=0; i<bits; i++) b.appendChild(div("bg"+i, 1, 1));
+// }
+// function div(id, w, h) {
+// var d=document.createElement("div");
+// d.style.position="absolute";
+// d.style.overflow="hidden";
+// d.style.width=w+"px";
+// d.style.height=h+"px";
+// d.setAttribute("id", id);
+// return (d);
+// }
+// function bang() {
+// var i, X, Y, Z, A=0;
+// for (i=0; i<bits; i++) {
+// X=Math.round(Xpos[i]);
+// Y=Math.round(Ypos[i]);
+// Z=document.getElementById("bg"+i).style;
+// if((X>=0)&&(X<swide)&&(Y>=0)&&(Y<shigh)) {
+// Z.left=X+"px";
+// Z.top=Y+"px";
+// }
+// if ((decay[i]-=1)>14) {
+// Z.width="3px";
+// Z.height="3px";
+// }
+// else if (decay[i]>7) {
+// Z.width="2px";
+// Z.height="2px";
+// }
+// else if (decay[i]>3) {
+// Z.width="1px";
+// Z.height="1px";
+// }
+// else if (++A) Z.visibility="hidden";
+// Xpos[i]+=dX[i];
+// Ypos[i]+=(dY[i]+=1.25/intensity);
+// }
+// if (A!=bits) setTimeout("bang()", speed);
+// }
+
+// function stepthrough() {
+// var i, Z;
+// var oldx=xpos;
+// var oldy=ypos;
+// xpos+=dx;
+// ypos-=4;
+// if (ypos<bangheight||xpos<0||xpos>=swide||ypos>=shigh) {
+// for (i=0; i<bits; i++) {
+// Xpos[i]=xpos;
+// Ypos[i]=ypos;
+// dY[i]=(Math.random()-0.5)*intensity;
+// dX[i]=(Math.random()-0.5)*(intensity-Math.abs(dY[i]))*1.25;
+// decay[i]=Math.floor((Math.random()*16)+16);
+// Z=document.getElementById("bg"+i).style;
+// Z.backgroundColor=colours[colour];
+// Z.visibility="visible";
+// }
+// bang();
+// launch();
+// }
+// document.getElementById("lg").style.left=xpos+"px";
+// document.getElementById("lg").style.top=ypos+"px";
+// document.getElementById("tg").style.left=oldx+"px";
+// document.getElementById("tg").style.top=oldy+"px";
+// }
+// function launch() {
+// colour=Math.floor(Math.random()*colours.length);
+// xpos=Math.round((0.5+Math.random())*swide*0.5);
+// ypos=shigh-5;
+// dx=(Math.random()-0.5)*4;
+// bangheight=Math.round((0.5+Math.random())*shigh*0.4);
+// document.getElementById("lg").style.backgroundColor=colours[colour];
+// document.getElementById("tg").style.backgroundColor=colours[colour];
+// }
+// window.onscroll=set_scroll;
+// function set_scroll() {
+// var sleft, sdown;
+// if (typeof(self.pageYOffset)=="number") {
+// sdown=self.pageYOffset;
+// sleft=self.pageXOffset;
+// }
+// else if (document.body.scrollTop || document.body.scrollLeft) {
+// sdown=document.body.scrollTop;
+// sleft=document.body.scrollLeft;
+// }
+// else if (document.documentElement && (document.documentElement.scrollTop || document.documentElement.scrollLeft)) {
+// sleft=document.documentElement.scrollLeft;
+// sdown=document.documentElement.scrollTop;
+// }
+// else {
+// sdown=0;
+// sleft=0;
+// }
+// var s=document.getElementById("bod").style;
+// s.top=sdown+"px";
+// s.left=sleft+"px";
+// }
+// window.onresize=set_width;
+// function set_width() {
+// if (typeof(self.innerWidth)=="number") {
+// swide=self.innerWidth;
+// shigh=self.innerHeight;
+// }
+// else if (document.documentElement && document.documentElement.clientWidth) {
+// swide=document.documentElement.clientWidth;
+// shigh=document.documentElement.clientHeight;
+// }
+// else if (document.body.clientWidth) {
+// swide=document.body.clientWidth;
+// shigh=document.body.clientHeight;
+// }
+// }
+// window.onload=function() { if (document.getElementById) {
+// set_width();
+// write_fire();
+// launch();
+// setInterval('stepthrough()', speed);
+// }}
+// //]]>
+// }
+
+
+//setInterval('partic()', 1000)
+
+function partic() {
+    const particleCount = 300; // Experimente alterar a contagem de partículas e veja o que acontece
+const colors = ["Pink", "White"]; // Você não está limitado a apenas 2 cores aqui!
+const particles = [];
+
+const canvas = document.getElementById('fireworkCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Particle {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.radius = Math.random() * 2 + 1;
+    this.velocity = {
+      x: Math.random() * 2 - 1,
+      y: Math.random() * 2 - 1
+    };
+    this.alpha = 1;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  update() {
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.alpha -= 0.01;
+    this.radius -= 0.01;
+    this.draw();
+  }
+}
+
+function createFirework(x, y) {
+  for (let i = 0; i < particleCount; i++) {
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    const particle = new Particle(x, y, color);
+    particles.push(particle);
+  }
+}
+
+
+}
+
