5e33ec59147bc8f864ed61018d7dda6309f754eb
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-12-11 06:27:41 -0300

    form

--- funcionamento.js
+++ funcionamento.js
@@ -5,7 +5,7 @@ const mes = data.getMonth() + 1
 const ano = data.getFullYear()
 var hora = data.getHours();
 
-var feriados = ["20/1/2023", "20/2/2023", "21/2/2023", "22/2/2023", "1/3/2023", "6/4/2023", "7/4/2023", "21/4/2023", "1/5/2023", "8/6/2023","9/6/2023",,"3/7/2023", "7/9/2023", "8/9/2023", "18/9/2023","12/10/2023","13/10/2023", "2/11/2023", "3/11/2023", "15/11/2023", "25/12/2023", "26/12/2023", "27/12/2023", "28/12/2023", "29/12/2023", "30/12/2023", "31/12/2023", "1/1/2024"]
+var feriados = ["20/1/2023", "20/2/2023", "21/2/2023", "22/2/2023", "1/3/2023", "6/4/2023", "7/4/2023", "21/4/2023", "1/5/2023", "8/6/2023","9/6/2023",,"3/7/2023", "7/9/2023", "8/9/2023", "18/9/2023","12/10/2023","13/10/2023", "2/11/2023", "3/11/2023", "15/11/2023","11/12/2023", "12/12/2023", "13/12/2023", "25/12/2023", "26/12/2023", "27/12/2023", "28/12/2023", "29/12/2023", "30/12/2023", "31/12/2023", "1/1/2024"]
 
 const anoCompara = `${dia}/${mes}/${ano}`
 //
@@ -27,7 +27,8 @@ for (let index = 0; index < feriados.length; index++) {
         funciona.innerHTML = `<div class="funcionamento cvermelho">
         <div >
             <i class="bi bi-emoji-frown-fill"></i>
-            <p>Hoje <strong> ${anoCompara} NÃO</strong> estamos funcionando - Tenha um ótimo FDS</p>
+            <p>Hoje <strong> ${anoCompara} NÃO</strong> estamos em funcionamento</p>
+            
         </div>
 
     </div>`
@@ -60,7 +61,7 @@ for (let index = 0; index < feriados.length; index++) {
         funciona.innerHTML = `<div class="funcionamento cvermelho">
         <div >
             <i class="bi bi-emoji-frown-fill"></i>
-            <p>Hoje ${anoCompara}<strong> NÃO</strong> estamos funcionando - Feriado</p>
+            <p>Hoje <strong> ${anoCompara} NÃO</strong> estamos em funcionamento</p>
             <button><a href="/horario-de-funcionamento.html">Veja o calendário completo</a></button>
         </div>
     </div>`
