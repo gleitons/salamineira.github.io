742ecaf1470627083eb9530d08555cb8337e18b9
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-01-28 12:09:01 -0300

    atualçiza

--- scripts/markup.js
+++ scripts/markup.js
@@ -1,5 +1,5 @@
-function calcularMarkup(){
-    
+function calcularMarkup() {
+
     const vCusto = document.getElementById('pc').value;
     const precoCusto = vCusto.replace(',', '.');
 
@@ -8,42 +8,49 @@ function calcularMarkup(){
 
     const dv = (document.getElementById('dv').value).replace(',', '.');
     const despesaVari = 100 * (porcentagem(dv));
-    
+
     const ml = (document.getElementById('ml').value).replace(',', '.');
     const margemLucro = 100 * (porcentagem(ml));
 
     // const margemLucro = mLucro.replace(',', '.');
 
     if (vCusto.length > 0) {
-        const Markup = 100/(100 - (despesaFixa + despesaVari + margemLucro));
+
+        const Markup = 100 / (100 - (despesaFixa + despesaVari + margemLucro));
         const precoVenda = precoCusto * Markup;
         if (precoVenda == precoCusto) {
             pVenda.innerHTML = `Adicione as despesas`;
         } else {
             pVenda.innerHTML = `R$ ${(precoVenda).toFixed(2)}`;
+            
         }
         resMarkup.innerText = (Markup).toFixed(2);
+
+        abreLink()
         
+
     } else {
         alert('Preencha o preço de Custo')
     }
 
-    
+
 
 }
 
 function porcentagem(valor) {
-    const porcent = valor/100;
+    const porcent = valor / 100;
     return porcent;
-    
+
 }
-/*
-const vCusto = document.getElementById('pc').value;
-    const precoCusto = vCusto.replace(',', '.');
-    const dFixa = 100 * (porcentagem(document.getElementById('df').value));
-    const despesaFixa = dFixa.replace(',', '.');
-    const dVari = 100 * (porcentagem(document.getElementById('dv').value));
-    const despesaVari = dVari.replace(',', '.');
-    const mLucro = 100 * (porcentagem(document.getElementById('ml').value));
-    const margemLucro = mLucro.replace(',', '.');
-    */
+
+function abreLink() {
+    const nomeProduto = document.querySelector('#nomeProduto').value
+    const pc = document.querySelector('#pc').value
+    const df = document.querySelector('#df').value
+    const dv = document.querySelector('#dv').value
+    const ml = document.querySelector('#ml').value
+    const vMarke = document.querySelector('#resMarkup').textContent
+    const pVenda = document.querySelector('#resMarkup').textContent
+
+    window.open(`./calculadora-mark.html#${nomeProduto}#${pc}#${df}#${dv}#${ml}#${vMarke}#${pVenda}', '_blank`);
+}
