5a9fff24596c1a1cac8d1a5757721440980d5860
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-03-05 22:08:54 -0300

    ok

--- conselho-2023/js.js
+++ conselho-2023/js.js
@@ -289,14 +289,13 @@ function fechaVotos() {
 }
 if (localStorage.getItem('aceitatermos') == 'sim') {
     document.querySelector('#termos').classList.add('dnone')
-} 
-// else {
-//     document.querySelector('#termos').classList.remove('dnone')
-// }
+} else {
+    document.querySelector('#termos').classList.remove('dnone')
+}
 
 function aceitatermos() {
     //document.querySelector('.votos').innerHTML = ``
-    localStorage.setItem('aceitatermos', 'sim')
+    localStorage.setItem('aceitatermos', 'nao')
     document.querySelector('#termos').classList.toggle('dnone')
     // if(localStorage.getItem('aceitatermos') == null) {
 
@@ -673,4 +672,4 @@ function createFirework(x, y) {
 function encerrado() {
     document.querySelector('.avisoencerrado').innerHTML += `<p>APP ENCERRADO COM SUCESSO!</p>`
 }
-//setInterval('encerrado()', 1000)
+setInterval('encerrado()', 1000)
