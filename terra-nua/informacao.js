4981e32f36b46ba63dc8e78f428313446d6922d1
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-03-27 21:43:36 -0300

    terra nua

--- terra-nua/informacao.js
+++ terra-nua/informacao.js
@@ -1,13 +1,9 @@
-
-
-
-
 function gerarNomes(anoS) {
-   // const url2016 = `./cidades-mg-${anoS}`
+    // const url2016 = `./cidades-mg-${anoS}`
     const cidadeC = document.querySelector('#cidadeC')
     const response = localStorage.getItem(`city${anoS}`);
     const data = JSON.parse(response)
-    console.log(data)
+    // console.log(data)
 
     data.map((nomeCity) => {
         const seletorCidade = document.createElement('option');
@@ -15,57 +11,45 @@ function gerarNomes(anoS) {
         seletorCidade.textContent = nomeCity.nome;
         cidadeC.appendChild(seletorCidade)
     })
-    
+
 }
+
+
 
 async function avaliacoes(anoS) {
     const url = `./cidades-mg-${anoS}`
-    
     const cidadeCC = document.querySelector('#cidadeC')
     var cidadeC = cidadeCC.options[cidadeCC.selectedIndex].value
 
     const tabelaAvaliacaoVenal = document.querySelector('#tabelaAvaliacaoVenal')
     const response = await fetch(url)
-    var data = await response.json()
+    var data = await response.json()    
 
-    function verMaior(a, b) {
-        //console.log(a.aptidaoBoa)
-        const aa = Number(a.aptidaoBoa.replace(/[^0-9]/g, ''))
-        const bb = Number(b.aptidaoBoa.replace(/[^0-9]/g, ''))
-        //console.log(aa)
-        //console.log(bb)
-        if (aa < bb) return 1;
-        if (aa > bb) return -1;
-        return 0;
+    localStorage.setItem(`city${anoS}`, JSON.stringify(data))
+
+    gerarNomes(anoS)
+
+    
+
+    const cidadeMaior = await data.sort(function (a, b) {
         
-    }
-    // function verMenor(c, d) {
-    //     //console.log(a.aptidaoBoa)
-    //     const cc = Number(c.aptidaoBoa.replace(/[^0-9]/g, ''))
-    //     const dd = Number(d.aptidaoBoa.replace(/[^0-9]/g, ''))
-    //     //console.log(aa)
-    //     //console.log(bb)
-    //     if (cc < dd) return -1;
-    //     if (cc > dd) return  1;
-    //     return 0;
+            
+            const aa = parseFloat(a.aptidaoBoa.toString().replace('.', ''))
+            const bb = parseFloat(b.aptidaoBoa.toString().replace('.', ''))
+            // console.log(aa) 
+            if (aa < bb) return 1;
+            if (aa > bb) return -1;
+            return 0;
         
-    // }
+        
+    })
    
-
-    var agoraN =  data.sort(verMaior)
-    //var menorValor = data.sort(verMenor)
-
-    //console.log(menorValor)
-
-    if(localStorage.getItem(`city${anoS}`) == null){
-        
-        localStorage.setItem(`city${anoS}`, JSON.stringify(data))
-    }
-
-    data.map((city) => {
-        if(cidadeC == city.nome) {
+   
+   
+    data.map((city) => {        
+        if (cidadeC == city.nome ) {
             const aptBoa = city.aptidaoBoa
-        tabelaAvaliacaoVenal.innerHTML = `
+            tabelaAvaliacaoVenal.innerHTML = `
         <h2>Preço por Ha (hectare)</h2>
         <div style="height:45px;">
         <img src="./imagens/emater-mg-logo-6FDB59CBDA-seeklogo.com_.png.webp" alt="">
@@ -132,16 +116,16 @@ async function avaliacoes(anoS) {
         <p class="cidadeDown" onclick="esconde()" >CLIQUE AQUI - Veja a Assinatura da Reunião em ${city.ano} em Lagoa dos Patos - MG</p>
         
         <p >Baixe a planilha completa - <a href="./arquivos/valor-terra-nua-${anoS}.pdf#">Clique Aqui</a></p>
-        <p class="curiosidadeS" ><strong>*CURIOSIDADE:</strong> Você sabia que a cidade com maior valor da terra nua em <strong>${city.ano} foi ${agoraN[0].nome}</strong>, com valor venal de aptidão Boa no valor de R$ <strong>${agoraN[0].aptidaoBoa} mil reais</strong></p>
+        <p class="curiosidadeS" ><strong>*CURIOSIDADE:</strong> Você sabia que a cidade com maior Valor da terra nua em Minas Gerais das cidades cadastradas acima: Em <strong>${city.ano} foi ${cidadeMaior[0].nome}</strong>, com valor venal de aptidão Boa no valor de  <strong class=""vtnMaior>R$ ${cidadeMaior[0].aptidaoBoa}</strong> mil reais por Hectare. A menor aptidão Boa foi a cidade de <strong>${cidadeMaior[data.length - 1].nome}</strong> com valor de terra nua, aptidão boa avaliado em <strong>R$ ${cidadeMaior[data.length - 1].aptidaoBoa}</strong> Reais</p>
         <div class="assinaturaTermo ocultaEsome" id="imgOclude">
         <img src="./img/terra-nua-${anoS}.jpg" onclick="esconde()" style="cursor:pointer;" alt="Valor de terra nua do município de ${city.nome} no ano de ${city.ano}">
         </div>
-    </div>`   
-        }       
-        
+    </div>`
+        }
+
     })
- gerarNomes(anoS)
     
+
 }
 
 function esconde() {
@@ -152,12 +136,12 @@ function esconde() {
 function gerarMenu() {
     var menuTopInfo = document.querySelector('#menuTopInfo')
 
-    for(let i = 2010; i <= 2023; i++) {
+    for (let i = 2010; i <= 2023; i++) {
         menuTopInfo.innerHTML += `<a href="./venal-${i}.html">
         <li>${i}</li>
         </a>`
     }
-   
+
 
 }
 gerarMenu()
@@ -181,6 +165,16 @@ function filtrarItens() {
         }
     }
 }
+function verMaior(a, b) {
+    //console.log(a.aptidaoBoa)
+    const aa = Number(a.aptidaoBoa)
+    const bb = Number(b.aptidaoBoa)
+    //console.log(aa)
+    //console.log(bb)
+    if (aa < bb) return 1;
+    if (aa > bb) return -1;
+    return 0;
 
+}
 // Adiciona um evento de input ao campo de pesquisa para chamar a função de filtragem
-document.getElementById("searchInput").addEventListener("input", filtrarItens);
+// document.getElementById("searchInput").addEventListener("input", filtrarItens);
