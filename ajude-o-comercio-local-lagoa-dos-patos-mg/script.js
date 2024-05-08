49f0dc4219290e12d331a4592da8f6b30de23b47
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-05-07 21:41:29 -0300

    ola

--- ajude-o-comercio-local-lagoa-dos-patos-mg/script.js
+++ ajude-o-comercio-local-lagoa-dos-patos-mg/script.js
@@ -1,5 +1,8 @@
 const btnBuscar = document.querySelectorAll('button')[0]
 
+function capitalizeFirstLetter(str) {
+  return str.substring(0, 1).toUpperCase() + str.substring(1);
+}
 
 btnBuscar.addEventListener('click', async () => {
     const cnpjInteiro =  document.querySelector('input').value
@@ -34,16 +37,34 @@ btnBuscar.addEventListener('click', async () => {
     inputs[6].value  = data.cep
     inputs[7].value  = data.razao_social.replace(/[0-9.]/g, '')
     const abrir = document.querySelector('form')
-
+    const dCNPJ = `${c[0]}${c[1]}.${c[2]}${c[3]}${c[4]}.${c[5]}${c[6]}${c[7]}/${c[8]}${c[9]}${c[10]}${c[11]}-${c[12]}${c[13]}`
     tds[2].textContent = data.razao_social
     tds[4].textContent = `${data.logradouro}, Nº ${data.numero}, Bairro ${data.bairro}`
-    tds[6].textContent = `${c[0]}${c[1]}.${c[2]}${c[3]}${c[4]}.${c[5]}${c[6]}${c[7]}/${c[8]}${c[9]}${c[10]}${c[11]}-${c[12]}${c[13]}`
+    tds[6].textContent = dCNPJ
     tds[8].textContent = data.municipio
     tds[10].textContent = data.uf
     tds[12].textContent = data.cep
     tds[14].textContent = data.razao_social.replace(/[0-9.]/g, '')
-
+    numeroCliente.value = data.ddd_telefone_1
     pp[14].textContent = data.razao_social.replace(/[0-9.]/g, '')
+    const pegaTexto = document.querySelector('#mensagemC').value
+    var pNome = ''
+    if(data.razao_social.includes('.')) {
+      pNome = data.razao_social.replace(/[0-9.]/g, '').split(' ')[1].toLowerCase()
+    } else {
+      pNome = data.razao_social.replace(/[0-9.]/g, '').split(' ')[0].toLowerCase()
+    }
+    
+    doFulano.textContent = capitalizeFirstLetter(pNome)
+    doCNPJ.textContent = dCNPJ
+    const textoTrocado = pegaTexto.replace('trocaNome', capitalizeFirstLetter(pNome) )
+    const numero = numeroCliente.value
+    document.querySelector('#mensagemC').value = textoTrocado
+    linkEnviar.href = `https://api.whatsapp.com/send?l=pt-BR&phone=55${numero}&text=${textoTrocado}`
+    linkEnviar.target ='_blank  '
+    linkEnviar.setAttribute('target', '_blank');
+
+    document.querySelector('#title').textContent = `${data.razao_social.replace(/[0-9.]/g, '')} - Apoio Radio Lagoa dos Patos MG - ${data.cnpj}`
 
     abrir.style.display = 'block'
 
