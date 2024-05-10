c8b5baa7b44b04bf22e271fe8bd380952a3093fa
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-05-10 09:53:35 -0300

    Update empreendedor-de-lagoa-dos-patos-mg.js

--- empreendedor-de-lagoa-dos-patos-mg.js
+++ empreendedor-de-lagoa-dos-patos-mg.js
@@ -2,10 +2,10 @@ const displayer = document.querySelector('.content')
 const menuSideBar = document.querySelector('#menu-sidebar ul')
 menuSideBar.innerHTML += `<li id="menu-item-222" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-66"><a href="https://salamineira.com">SALA MINEIRA DO EMPREENDEDOR</a></li>`
 
-const div = document.createElement('div')
-const link = document.createElement('a')
-div.textContent = ('olá mundo')
-link.href = 'https://salamineira.com'
-link.textContent = "Visite a sala mineira"
-div.appendChild(link)
-displayer.appendChild(div)
+// const div = document.createElement('div')
+// const link = document.createElement('a')
+// div.textContent = ('olá mundo')
+// link.href = 'https://salamineira.com'
+// link.textContent = "Visite a sala mineira"
+// div.appendChild(link)
+// displayer.appendChild(div)
