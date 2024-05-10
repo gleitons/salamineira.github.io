2f1498d19809589d83c3b32ed05a4f2592001c36
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-05-10 09:39:29 -0300

    ola

--- empreendedor-de-lagoa-dos-patos-mg.js
+++ empreendedor-de-lagoa-dos-patos-mg.js
@@ -1,2 +1,10 @@
-const url = "link"
-console.log(url)
+const displayer = document.querySelector('.content')
+
+const div = document.createElement('div')
+const link = document.createElement('a')
+div.textContent = ('olá mundo')
+link.href = 'https://salamineira.com'
+link.textContent = "Visite a sala mineira"
+div.appendChild(link)
+console.log(displayer)
+displayer.appendChild(div)
