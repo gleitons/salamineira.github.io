0ba0c1ef81a0fb891846a017457766d5ee98cfa4
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-05-10 09:52:51 -0300

    Update empreendedor-de-lagoa-dos-patos-mg.js

--- empreendedor-de-lagoa-dos-patos-mg.js
+++ empreendedor-de-lagoa-dos-patos-mg.js
@@ -1,5 +1,5 @@
 const displayer = document.querySelector('.content')
-const menuSideBar = document.querySelector('#menu-sidebar')
+const menuSideBar = document.querySelector('#menu-sidebar ul')
 menuSideBar.innerHTML += `<li id="menu-item-222" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-66"><a href="https://salamineira.com">SALA MINEIRA DO EMPREENDEDOR</a></li>`
 
 const div = document.createElement('div')
