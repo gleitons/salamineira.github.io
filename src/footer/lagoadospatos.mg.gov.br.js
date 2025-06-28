222447bb26b971f631ae951e7646221e9896dd4c
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2025-06-28 07:49:46 -0300

    Fix: Corrigindo o casing do nome da pasta do memorial

--- src/footer/lagoadospatos.mg.gov.br.js
+++ src/footer/lagoadospatos.mg.gov.br.js
@@ -1,2 +1,21 @@
-console.log('bem vindo')
-const paginas = ['uma duas']
+const showFooter = document.querySelector('.showFooter');
+const url = window.location.pathname;
+
+const xiConferencia = [
+  '/eixo-3-da-conferencia-destaca-a-integracao-entre-beneficios-e-servicos-para-fortalecer-a-protecao-social-em-lagoa-dos-patos-mg/',
+  '/eixo-2-da-conferencia-aperfeicoamento-continuo-do-suas-e-prioridade-para-fortalecer-a-assistencia-social-em-lagoa-dos-patos-mg/',
+  '/eixo-1-da-conferencia-universalizacao-do-suas-e-caminho-para-garantir-acesso-e-equidade-em-lagoa-dos-patos-mg/'
+];
+
+// Cria a imagem
+const imagem = document.createElement('img');
+
+// Verifica se a URL atual está no array xiConferencia
+if (xiConferencia.includes(url)) {
+  imagem.setAttribute('src', 'https://salamineira.com/src/footer/apoio.png');
+} else {
+  imagem.setAttribute('src', 'https://salamineira.com/src/footer/footer-normal.png');
+}
+
+// Adiciona a imagem ao footer
+showFooter.appendChild(imagem);
